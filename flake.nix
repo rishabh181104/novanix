d0bb7280d7953446a329aefdabacc2b8760f2fb8
rishabh181104 <rishabhhaldiya18@gmail.com>
2025-07-05 04:36:07 +0530

    s

--- flake.nix
+++ flake.nix
@@ -15,60 +15,53 @@
 
   outputs = { self, nixpkgs, home-manager, stylix, ... } @ inputs:
     let
-      # Centralized configuration - define once, use everywhere
-      shared = {
-        hostName = "novanix";     # Change to your hostname
+# Centralized configuration - define once, use everywhere
+    shared = {
+      hostName = "novanix";     # Change to your hostname
         userName = "nova";          # Change to your username
         system = "x86_64-linux";    # Change architecture if needed
         theme = "dracula";
-        timeZone = "Asia/Kolkata";
-        # wallpaper = ./themes/${theme}/background.png;
-      };
-      lib = nixpkgs.lib;
-      pkgs = nixpkgs.legacyPackages.${shared.system};
-    in {
-      nixosConfigurations.${shared.hostName} = lib.nixosSystem {
-        inherit (shared) system;
-        specialArgs = { inherit shared inputs; };  # Make shared available in all modules
+      timeZone = "Asia/Kolkata";
+# wallpaper = ./themes/${theme}/background.png;
+    };
+  lib = nixpkgs.lib;
+  pkgs = nixpkgs.legacyPackages.${shared.system};
+  in {
+    nixosConfigurations.${shared.hostName} = lib.nixosSystem {
+      inherit (shared) system;
+      specialArgs = { inherit shared inputs; };  # Make shared available in all modules
 
         modules = [
-          ({ shared, ... }: { 
-            networking.hostName = shared.hostName;
-          })
+        ({ shared, ... }: { 
+         networking.hostName = shared.hostName;
+         })
 
-          # Import your configuration files
-          ./configuration.nix
-          ./hardware.nix
+# Import your configuration files
+      ./configuration.nix
+        ./hardware.nix
 
-          # Home Manager integration
-          home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              extraSpecialArgs = { inherit shared inputs; };
-              users.${shared.userName} = {
-                imports = [ 
-                  ./home.nix
-                ];
-              };
+# Home Manager integration
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            extraSpecialArgs = { inherit shared inputs; };
+            users.${shared.userName} = {
+              imports = [ 
+                ./home.nix
+              ];
             };
-          }
+          };
+        }
 
-          # Stylix theming (uncomment when ready)
-          # stylix.nixosModules.stylix
-          # {
-          #   stylix.image = shared.wallpaper;
-          #   stylix.base16Scheme = ...;
-          # }
-        ];
-      };
-      
-      # Standalone Home Manager configuration
-      homeConfigurations.${shared.userName} = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        extraSpecialArgs = { inherit shared; };
-        modules = [ ./home.nix ];
-      };
+# Stylix theming (uncomment when ready)
+# stylix.nixosModules.stylix
+# {
+#   stylix.image = shared.wallpaper;
+#   stylix.base16Scheme = ...;
+# }
+      ];
     };
+  };
 }
