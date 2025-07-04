03ca93f45c7b5cb5d8645b590dd08013fa5c154d
rishabh181104 <rishabhhaldiya18@gmail.com>
2025-07-05 02:08:10 +0530

    s

--- hardware.nix
+++ hardware.nix
@@ -14,38 +14,38 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/447ad0fd-831f-4ff3-b2a4-d1a6a86416f0";
-      fsType = "ext4";
-    };
+  { device = "/dev/disk/by-uuid/447ad0fd-831f-4ff3-b2a4-d1a6a86416f0";
+    fsType = "ext4";
+  };
 
   fileSystems."/media" =
-    { device = "systemd-1";
-      fsType = "autofs";
-    };
-
-  fileSystems."/boot/efi" =
-    { device = "/dev/disk/by-uuid/AE91-76DE";
-      fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
-    };
+  { device = "systemd-1";
+    fsType = "autofs";
+  };
 
   fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/31d6b9e8-5112-462d-a905-cd7037e16c2c";
-      fsType = "ext4";
-    };
+  { device = "/dev/disk/by-uuid/31d6b9e8-5112-462d-a905-cd7037e16c2c";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot" =
+  { device = "/dev/disk/by-uuid/AE91-76DE";
+    fsType = "vfat";
+    options = [ "fmask=0022" "dmask=0022" ];
+  };
 
   swapDevices =
     [ { device = "/dev/disk/by-uuid/2d5a847d-6196-46b7-9dc3-11df8901de32"; }
     ];
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
+# Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+# (the default) this is the recommended approach. When using systemd-networkd it's
+# still possible to use this option, but it's recommended to use it in conjunction
+# with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+    networking.useDHCP = lib.mkDefault true;
+# networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
+# networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
 
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+    hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
