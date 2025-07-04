5ab3781deea3e49a03837ac1241f1b93d0b4ec0c
rishabh181104 <rishabhhaldiya18@gmail.com>
2025-07-04 07:32:39 +0530

    s

---
+++ home.nix
@@ -0,0 +1,77 @@
+{ config, pkgs, userName, homeDirectory, ... }:
+
+{
+
+  home.username = userName;
+  home.homeDirectory = homeDirectory;
+
+  xdg.configFile = {
+    "hypr" = {
+      source = ./dotfiles/hypr;
+      recursive = true;
+    };
+    "alacritty" = {
+      source = ./dotfiles/alacritty;
+      recursive = true;
+    };
+    "kitty" = {
+      source = ./dotfiles/kitty;
+      recursive = true;
+    };
+    "fish" = {
+      source = ./dotfiles/fish;
+      recursive = true;
+    };
+    "fastfetch" = {
+      source = ./dotfiles/fastfetch;
+      recursive = true;
+    };
+    "nvim" = {
+      source = ./dotfiles/nvim;
+      recursive = true;
+    };
+    "rofi" = {
+      source = ./dotfiles/rofi;
+      recursive = true;
+    };
+    "waybar" = {
+      source = ./dotfiles/waybar;
+      recursive = true;
+    };
+    "wlogout" = {
+      source = ./dotfiles/wlogout;
+      recursive = true;
+    };
+    "qtile" = {
+      source = ./dotfiles/qtile;
+      recursive = true;
+    };
+    "picom" = {
+      source = ./dotfiles/picom;
+      recursive = true;
+    };
+    "mako" = {
+      source = ./dotfiles/mako;
+      recursive = true;
+    };
+  };
+
+# Files for home directory
+  home.file = {
+#   ".bashrc".source = ./dotfiles/bashrc;
+#   ".tmux.conf".source = ./dotfiles/tmux.conf;
+    "starship.toml".source = ./dotfiles/starship.toml
+  };
+
+  home.stateVersion = "25.05"; # Please read the comment before changing.
+
+    home.packages = with pkgs; [
+    hello
+    ];
+
+  home.sessionVariables = {
+# EDITOR = "emacs";
+  };
+
+  programs.home-manager.enable = true;
+}
