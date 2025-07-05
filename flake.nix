3dc5ff884605a3e08c59987833aef73cf227c640
rishabh181104 <rishabhhaldiya18@gmail.com>
2025-07-06 04:53:48 +0530

    s

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "FLAKE FOR NOVA";
 
-  outputs = inputs@{ self,nixpkgs, ... }:
+  outputs = inputs@{ self,nixpkgs,stylix, ... }:
     let
 # ---- SYSTEM SETTINGS ---- #
     systemSettings = {
@@ -29,35 +29,16 @@
     font = "MesloLGS Nerd Font Mono";
     editor = "zen-editor";
   };
-
-  pkgs-unstable = import inputs.nixpkgs-patched {
-    system = systemSettings.system;
-    config = {
-      allowUnfree = true;
-      allowUnfreePredicate = (_: true);
-    };
-    overlays = [ inputs.rust-overlay.overlays.default ];
-  };
-
   lib = nixpkgs.lib;
-
-# Systems that can run tests:
-  supportedSystems = [ "aarch64-linux" "i686-linux" "x86_64-linux" ];
-
-# Function to generate a set based on supported systems:
-  forAllSystems = inputs.nixpkgs.lib.genAttrs supportedSystems;
-
-# Attribute set of nixpkgs for each system:
-  nixpkgsFor =
-    forAllSystems (system: import inputs.nixpkgs { inherit system; });
+  supportedSystems = [ "x86_64-linux" ];
   in {
-
     nixosConfigurations = {
       system = lib.nixosSystem {
         system = systemSettings.system;
         modules = [
           ./configuration.nix
-        ]; # load configuration.nix from selected PROFILE
+            stylix.nixosSystem.stylix
+        ];
         specialArgs = {
           inherit systemSettings;
           inherit userSettings;
@@ -65,24 +46,12 @@
         };
       };
     };
-    packages = forAllSystems (system:
-        let pkgs = nixpkgsFor.${system};
-        in {
-        default = self.packages.${system}.install;
-
-        install = pkgs.writeShellApplication {
-        name = "install";
-        runtimeInputs = with pkgs; [ git ]; # I could make this fancier by adding other deps
-        text = ''${./install.sh} "$@"'';
-        };
-        });
-
-    inputs = {
-      nixpkgs.url = "nixpkgs/nixos-unstable";
-      stylix = {
-        url = "github:danth/stylix";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
+  };
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    stylix = {
+      url = "github:danth/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 }
