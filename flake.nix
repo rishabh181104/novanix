08c4afdbd2e16094bb800eb48ad8f09b9c626ce5
rishabh181104 <rishabhhaldiya18@gmail.com>
2025-07-06 05:24:08 +0530

    s

--- flake.nix
+++ flake.nix
@@ -1,54 +1,77 @@
 {
-  description = "FLAKE FOR NOVA";
+  description = "My NixOS Flake";
 
-  outputs = inputs@{ self,nixpkgs,stylix, ... }:
-    let
+  outputs = { self, nixpkgs, stylix, ... }@inputs: {
+    let {
 # ---- SYSTEM SETTINGS ---- #
-    systemSettings = {
-      system = "x86_64-linux";
-      hostname = "novanix";
-      timezone = "Asia/Kolkata";
-      locale = "en_IN.UTF-8";
-      bootMode = "uefi";
-      bootMountPath = "/boot";
-      grubDevice = "";
-      gpuType = "nvidia";
-    };
+      systemSettings = {
+        system = "x86_64-linux";
+        hostname = "novanix";
+        timezone = "Asia/Kolkata";
+        locale = "en_IN/UTF-8";
+        bootMode = "uefi";
+        bootMountPath = "/boot";
+        grubDevice = "";
+        gpuType = "nvidia";
+      };
 
 # ----- USER SETTINGS ----- #
-  userSettings = rec {
-    username = "nova";
-    name = "Rishabh";
-    email = "rishabhhaldiya18@gmail.com";
-    dotfilesDir = "~/novanix";
-    theme = "dracula";
-    wm = "hyprland";
-    wmType = if ((wm == "hyprland") || (wm == "plasma")) then "wayland" else "x11";
-    browser = "brave";
-    term = "alacritty";
-    font = "MesloLGS Nerd Font Mono";
-    editor = "zen-editor";
-  };
-  lib = nixpkgs.lib;
-  supportedSystems = [ "x86_64-linux" ];
-  in {
-    nixosConfigurations = {
-      system = lib.nixosSystem {
-        system = systemSettings.system;
-        modules = [
-          ./configuration.nix
-            stylix.nixosSystem.stylix
-        ];
-        specialArgs = {
-          inherit systemSettings;
-          inherit userSettings;
-          inherit inputs;
+      userSettings = rec {
+        username = "nova";
+        name = "rishabh181104";
+        email = "rishabhhaldiya18@gmail.com";
+        dotfilesDir = "~/novanix";
+        theme = "io";
+        wm = "hyprland";
+        wmType = if ((wm == "hyprland") || (wm == "plasma")) then "wayland" else "x11";
+        browser = "brave";
+        term = "alacritty";
+        editor = "neovide";
+      }
+    };
+    in {
+      nixosConfigurations = {
+        systemSettings.hostname = nixpkgs.lib.nixosSystem {
+          systemSettings.system;
+          modules = [
+            ./configuration.nix
+# ({ pkgs, ... }: {
+#  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+#  environment.systemPackages = with pkgs; [
+#  wget curl git htop neovim tmux
+#  firefox vscode libreoffice
+#  mpv feh pavucontrol
+#  ];
+#  })
+              stylix.nixosModules.stylix
+# ({ pkgs, ... }: {
+#  stylix = {
+#  image = ./wallpaper.jpg; # Replace with your wallpaper
+#  base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+#  fonts = {
+#  serif = {
+#  package = pkgs.dejavu_fonts;
+#  name = "DejaVu Serif";
+#  };
+#  sansSerif = {
+#  package = pkgs.dejavu_fonts;
+#  name = "DejaVu Sans";
+#  };
+#  monospace = {
+#  package = pkgs.jetbrains-mono;
+#  name = "JetBrains Mono";
+#  };
+#  };
+#  };
+#  })
+              ];
         };
       };
     };
   };
+
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     stylix = {
       url = "github:danth/stylix";
       inputs.nixpkgs.follows = "nixpkgs";
