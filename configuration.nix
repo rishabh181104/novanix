9ece3db27d70ca348208b76a9cc6b8705965e5e0
rishabh181104 <rishabhhaldiya18@gmail.com>
2025-07-05 22:07:16 +0530

    s

--- configuration.nix
+++ configuration.nix
@@ -27,4 +27,3 @@
   system.stateVersion = "25.05"; # Did you read the comment?
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
 }
-
--- modules/network.nix
+++ modules/network.nix
@@ -2,17 +2,17 @@
 
 {
   i18n = {
-    defaultLocale = ${shared.locale};
+    defaultLocale = shared.locale;
     extraLocaleSettings = {
-      LC_ADDRESS = ${shared.locale};
-      LC_IDENTIFICATION = ${shared.locale};
-      LC_MEASUREMENT = ${shared.locale};
-      LC_MONETARY = ${shared.locale};
-      LC_NAME = ${shared.locale};
-      LC_NUMERIC = ${shared.locale};
-      LC_PAPER = ${shared.locale};
-      LC_TELEPHONE = ${shared.locale};
-      LC_TIME = ${shared.locale};
+      LC_ADDRESS = shared.locale;
+      LC_IDENTIFICATION = shared.locale;
+      LC_MEASUREMENT = shared.locale;
+      LC_MONETARY = shared.locale;
+      LC_NAME = shared.locale;
+      LC_NUMERIC = shared.locale;
+      LC_PAPER = shared.locale;
+      LC_TELEPHONE = shared.locale;
+      LC_TIME = shared.locale;
     };
   };
   networking.hostName = shared.hostName;
