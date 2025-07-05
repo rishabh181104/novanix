4317cc738dc1aec13f928d72f19d3bf4a66f68a3
rishabh181104 <rishabhhaldiya18@gmail.com>
2025-07-06 01:49:02 +0530

    s

--- flake.nix
+++ flake.nix
@@ -7,10 +7,6 @@
       url = "github:danth/stylix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nix-colors = {
-      url = "github:misterio77/nix-colors";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = { self, nixpkgs, stylix, ... } @ inputs:
--- modules/stylix.nix
+++ modules/stylix.nix
@@ -1,8 +1,10 @@
-{ lib, shared, pkgs, ... }:
+{ lib, shared, pkgs, stylix, ... }:
 {
   stylix = {
-    image = /home/${shared.userName}/novanix/themes/${shared.theme}/background.yaml;
-    base16Scheme = /home/${shared.userName}/novanix/themes/${shared.theme}/background.png;
+# image = /home/${shared.userName}/novanix/themes/${shared.theme}/background.yaml;
+# base16Scheme = /home/${shared.userName}/novanix/themes/${shared.theme}/background.png;
+    image = /home/nova/novanix/themes/uwunicorn/background.png;
+    base16Scheme = /home/nova/novanix/themes/uwunicorn/background.yaml;
     polarity = "dark";  # or "light"
       fonts = {
         monospace = {
